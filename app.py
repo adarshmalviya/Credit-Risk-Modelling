8f31ae0f6f46651618b2f2ea3d46e01c404e6a13
Adarsh Malviya <56603301+adarshmalviya@users.noreply.github.com>
2021-04-11 13:45:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__, template_folder='templates')
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/', methods = ["GET",'POST'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html')
+
+    if request.method == 'POST':
+        try:
+            int_features = [[float(x) for x in request.form.values()]]
+            if model.predict(np.array(int_features))[0] == 0:
+                prediction = "Good Customer for Bank"
+            else:
+                prediction = "Not a good Customer for Bank"
+        except:
+            prediction = "Invalid Data"
+
+        return render_template('main.html', prediction_text=prediction)
+    
+app.run()
