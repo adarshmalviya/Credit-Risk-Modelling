1e77ef1a42d2c4adfd5b5dadfab6c885b5007ac7
Adarsh Malviya <56603301+adarshmalviya@users.noreply.github.com>
2021-04-11 13:58:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ import pickle
 app = Flask(__name__, template_folder='templates')
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/', methods = ["GET",'POST'])
-def main():
+@app.route('/')
+def home():
+    return render_template('credit_model.html')
+
+@app.route('/credit_risk_model', methods = ["GET",'POST'])
+def predict():
     if request.method == 'GET':
-        return render_template('main.html')
+        return render_template('credit_model.html')
 
     if request.method == 'POST':
         try:
@@ -20,6 +24,6 @@ def main():
         except:
             prediction = "Invalid Data"
 
-        return render_template('main.html', prediction_text=prediction)
+        return render_template('credit_model.html', prediction_text=prediction)
     
-app.run()
+app.run(debug=True)
