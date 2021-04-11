7a34f3e9da37b5d34967a7e761aa54252507538c
Adarsh Malviya <56603301+adarshmalviya@users.noreply.github.com>
2021-04-11 22:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def predict():
             prediction = "Invalid Data"
 
         return render_template('credit_model.html', prediction_text=prediction)
-    
-app.run()
+if __name__ == "__main__":
+    app.run()
